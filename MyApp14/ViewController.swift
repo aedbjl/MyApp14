6910c3e97c50df950f23f7fea6c76d2b3482fd89
iii-user <iii-user@iii-userde-macbook-4.local>
2017-06-30 14:19:26 +0800

    w

--- MyApp14/ViewController.swift
+++ MyApp14/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
+import JavaScriptCore
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , BradProtocol{
 
+    @IBOutlet weak var webView: UIWebView!
+    
+    
+    @IBOutlet weak var labelName: UILabel!
+    
+    @IBAction func test1(_ sender: Any) {
+        
+        let url = Bundle.main.url(forResource: "brad", withExtension: "html")
+        let req = URLRequest(url: url!)
+        webView.loadRequest(req)
+        
+        let jsContext = webView.value(forKeyPath: "documentView.webView.mainFrame.javaScriptContext") as! JSContext
+        
+        jsContext.setObject(self, forKeyedSubscript: "iii" as (NSCopying & NSObjectProtocol)!)
+        
+    }
+    func m1(_ name:String){
+        print("ur name is \(name)")
+        labelName.text = name
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+   
 
 }
-
+@objc protocol BradProtocol :JSExport{
+    func m1(_ name:String)
+}
